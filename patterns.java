ab02ea7c34a5495bbbeb3604d2b057bc31294099
Rahul S <rahulsri@gmail.com>
2023-01-28 18:22:18 +0530

    13

--- patterns.java
+++ patterns.java
@@ -265,16 +265,17 @@ public class patterns{
     public static void pattern12(int n) {
         for(int i=0;i<n;i++){
             for(int j=0;j<=i;j++){
-                System.out.print(j+1);
+                System.out.print(j+1+" ");
             }
-            for (int j = 0; j < n*2-2-(2*i); j++) {
+            for (int j = 0; j < (n*3+1)-(4*i); j++) {
                 System.out.print(" ");
             }
             for (int j = i+1; j>0; j--) {
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.print("\n");
         }
+    }
         
     }
-}
+
